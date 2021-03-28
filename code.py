ed2a4f2a19443fbeb24885f1cb4f394361135b16
paladinescamila <camilapaladines27@gmail.com>
2021-03-28 11:46:50 -0500

    Add example

--- code.py
+++ code.py
@@ -56,14 +56,17 @@ def ecuaciones_normales(n, t, y):
             usando los datos de t (entrada) & y (salida).
     """
 
+    # Ajuste de datos
     m = len(t)
     A = [[t[i]**j for j in range(n)] for i in range(m)]
 
+    # Transpuestas y descomposición de Cholesky
     AT = np.transpose(A)
     A = np.matmul(AT, A)
     L = np.linalg.cholesky(A)
     LT = np.transpose(L)
 
+    # Solución de los sistemas triangulares
     ye = sucesiva_hacia_adelante(L, np.matmul(AT, y))
     x = sucesiva_hacia_atras(LT, ye)
 
@@ -78,29 +81,34 @@ def householder(n, t, y):
             usando los datos de t (entrada) & y (salida).
     """
 
+    # Ajuste de datos
     m = len(t)
     A = [[t[i]**j for j in range(n)] for i in range(m)]
     b = [i for i in y]
 
     for i in range(n):
 
+        # Obtención de a y alfa
         a = [A[j][i] for j in range(m)]
         alfa = 0
         for j in a: alfa += j**2
         alfa = alfa**0.5
 
+        # Cómputo de v
         if (A[i][i] > 0): alfa *= -1
         v = [0 for _ in range(i)]
         for j in range(i, m):
             if (j == i): v += [a[j] - alfa]
             else: v += [a[j]]
 
+        # Cómputo de H en A
         for k in range(n):
             vTx, vTv = 0, 0
             for j in range(m): vTx += v[j] * A[j][k]
             for j in range(m): vTv += v[j] * v[j]
             for j in range(i, m): A[j][k] = A[j][k] - 2 * (vTx/vTv) * v[j]
-        
+
+        # Cómputo de H en b
         vTx, vTv = 0, 0
         for j in range(m): vTx += v[j] * b[j]
         for j in range(m): vTv += v[j] * v[j]
@@ -124,41 +132,45 @@ def polinomio(n, t, x):
 
 
 # Dibuja los puntos y función resultante en el plano
-def paint_example(n, t, y):
+def paint_example(n, t, y, method):
+
+    # Obtención de x
+    if (method == 1):
+        start = time.time()
+        x = ecuaciones_normales(n, t, y)
+        time_ = time.time() - start
+    else:
+        start = time.time()
+        x = householder(n, t, y)
+        time_ = time.time() - start
+    
+    # Datos de validación
     m, min_t, max_t = len(t), min(t), max(t)
     t_ = np.linspace(min_t, max_t, 1000)
+    y_ = [polinomio(n, i, x) for i in t_]
 
-    start = time.time()
-    xe = ecuaciones_normales(n, t, y)
-    e_time = time.time() - start
-    ye = [polinomio(n, i, xe) for i in t_]
-    print("Método de Ecuaciones Normales")
-    print("x = {0}".format([round(i, 3) for i in xe]))
-    print("Tiempo = {0}".format(round(e_time, 5)))
-    for i in range(m): plt.plot(t[i], y[i], marker="o", color="black")
-    plt.plot(t_, ye, color="blue")
-    plt.grid()
-    plt.show()
+    # Muestra el resultado de x y el tiempo de ejecución
+    if (method == 1): plt.title("Método de Ecuaciones Normales")
+    else: plt.title("Método de Householder")
+    print("x = {0}".format([round(i, 3) for i in x]))
+    print("Tiempo = {0}".format(round(time_, 5)))
 
-    start = time.time()
-    xh = householder(n, t, y)
-    h_time = time.time() - start
-    yh = [polinomio(n, i, xh) for i in t_]
-    print("Método de Householder")
-    print("x = {0}".format([round(i, 5) for i in xh]))
-    print("Tiempo = {0}".format(round(h_time, 5)))
-    for i in range(m): plt.plot(t[i], y[i], marker="o", color="black")
-    plt.plot(t_, yh, color="red")
+    # Muestra la gráfica de los conjuntos de entrenamiento y validación
+    for i in range(m): plt.plot(t[i], y[i], marker="o", color="blue")
+    plt.plot(t_, y_, color="black")
+    plt.xlabel('t')
+    plt.ylabel('y')
     plt.grid()
     plt.show()
 
 
-# EJEMPLOS DE PRUEBA (También se encuentran en el informe)
+# EJEMPLO DE PRUEBA (También se encuentra en el informe)
 def main():
     n = 3
     t = [-1, -0.5, 0, 0.5, 1]
     y = [1, 0.5, 0, 0.5, 2]
-    paint_example(n, t, y)
+    paint_example(n, t, y, 1)
+    paint_example(n, t, y, 2)
 
 
 main()
